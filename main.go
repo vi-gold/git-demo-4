bc7dc36e18113b9227abe21f287ad4fc924187c4
vi-gold <popovpavelwork@gmail.com>
2025-11-27 09:18:54 +0500

    third commit

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand/v2"
+	"net/url"
 )
 
 var letterRune = []rune("qwertyuiopasdfghjklzxcvbnm0123456789!@#$%^&-")
@@ -26,6 +28,28 @@ func (acc *account) generatePassword(length int) {
 	acc.password = string(pass)
 }
 
+func newAccount(login, password, urlString string) (*account, error) {
+	_, err := url.ParseRequestURI(urlString)
+	if login == "" {
+		return nil, errors.New("EMPTY_LOGIN")
+	}
+	if err != nil {
+		return nil, errors.New("IVALID_URL")
+	}
+	newAcc := &account{
+		login:    login,
+		password: password,
+		url:      urlString,
+	}
+	if newAcc.password == "" {
+		var length int
+		fmt.Print("\nВведите длинну пароля: ")
+		fmt.Scan(&length)
+		newAcc.generatePassword(length)
+	}
+	return newAcc, nil
+}
+
 func main() {
 	fmt.Println(rand.IntN(10))
 
@@ -35,19 +59,14 @@ func main() {
 	}
 
 	login := promptData("Введите логин")
-	//password := promptData("Введите пароль")
+	password := promptData("Введите пароль")
 	url := promptData("Введите URL")
 
-	myAccount := account{
-		login: login,
-		//	password: password,
-		url: url,
+	myAccount, err := newAccount(login, password, url)
+	if err != nil {
+		fmt.Println("Неверный формат URL или Login")
+		return
 	}
-
-	var length int
-	fmt.Print("\nВведите длинну пароля: ")
-	fmt.Scan(&length)
-	myAccount.generatePassword(length)
 	myAccount.outputPassword()
 	// 8.4 Использование указателей
 	// a := 5
@@ -63,7 +82,7 @@ func main() {
 func promptData(prompt string) string {
 	fmt.Print(prompt + ": ")
 	var res string
-	fmt.Scan(&res)
+	fmt.Scanln(&res)
 	return res
 }
 
